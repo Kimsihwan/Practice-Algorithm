0b834745086518083652c41bd2d57727a93398de
Sihwan-Kim <namid882@gmail.com>
2021-12-09 13:22:50 +0900

    feat: [bj, 5339] 콜센터
    https://www.acmicpc.net/problem/5339

---
+++ baekjoon/BOJ_5339.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : 콜센터
+ * url : https://www.acmicpc.net/problem/5339
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_5339 {
+
+	public static void main(String[] args) {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+}
