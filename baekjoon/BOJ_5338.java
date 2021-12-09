6e4a02dee25cf1b4e77a083da8b74e01b33e7f50
Sihwan-Kim <namid882@gmail.com>
2021-12-09 13:17:20 +0900

    feat: [bj, 5338] 마이크로소프트 로고
    https://www.acmicpc.net/problem/5338

---
+++ baekjoon/BOJ_5338.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : 마이크로소프트 로고
+ * url : https://www.acmicpc.net/problem/5338
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_5338 {
+
+	public static void main(String[] args) {
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+}
