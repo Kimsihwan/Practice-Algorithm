234407ad4024d19b198e8c024de05d68a8022463
Sihwan-Kim <namid882@gmail.com>
2021-12-09 13:12:10 +0900

    feat: [bj, 5337] 웰
    https://www.acmicpc.net/problem/5337

---
+++ baekjoon/BOJ_5337.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : 웰컴
+ * url : https://www.acmicpc.net/problem/5337
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_2338 {
+
+	public static void main(String[] args) {
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
