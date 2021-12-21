d289939473ad8b0681a36f5cebb7dbbcc50b556a
Sihwan-Kim <namid882@gmail.com>
2021-12-21 20:59:10 +0900

    feat: [bj, 11942] 고려대학교는 사랑입니다.
    https://www.acmicpc.net/problem/11942

---
+++ baekjoon/BOJ_11942.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : 고려대는 사랑입니다
+ * url : https://www.acmicpc.net/problem/11942
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_9654 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("고려대학교");
+	}
+}
