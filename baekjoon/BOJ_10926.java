2d301c1fda45f7800de8a3a620134fc667071a71
Sihwan-Kim <namid882@gmail.com>
2021-12-21 20:44:42 +0900

    feat: [bj, 10926] ??!
    https://www.acmicpc.net/problem/10926

---
+++ baekjoon/BOJ_10926.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : ??!
+ * url : https://www.acmicpc.net/problem/10926
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_10926 {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+
+		String next = sc.next();
+		System.out.println(next+"??!");
+		sc.close();
+	}
+}
