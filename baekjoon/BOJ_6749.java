e34d0620ab473e3716e2c4948049a82bd47bc849
Sihwan-Kim <namid882@gmail.com>
2021-12-21 19:48:01 +0900

    feat: [bj, 6749] Next in line
    https://www.acmicpc.net/problem/6749

---
+++ baekjoon/BOJ_6749.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : Next in line
+ * url : https://www.acmicpc.net/problem/6749
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_5339 {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+
+		System.out.println((a + b + c + d) / 60);
+		System.out.println((a + b + c + d) % 60);
+		sc.close();
+	}
+}
