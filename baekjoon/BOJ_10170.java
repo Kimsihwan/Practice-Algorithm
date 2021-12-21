03501be508df291922f5453ccdc52a9d47bf1503
Sihwan-Kim <namid882@gmail.com>
2021-12-21 20:36:15 +0900

    feat: [bj, 10170] NFC West vs North
    https://www.acmicpc.net/problem/10170

---
+++ baekjoon/BOJ_10170.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : NFC West vs North
+ * url : https://www.acmicpc.net/problem/10170
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_10170 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("NFC West       W   L  T");
+		System.out.println("-----------------------");
+		System.out.println("Seattle        13  3  0");
+		System.out.println("San Francisco  12  4  0");
+		System.out.println("Arizona        10  6  0");
+		System.out.println("St. Louis      7   9  0");
+		System.out.println();
+		System.out.println("NFC North      W   L  T");
+		System.out.println("-----------------------");
+		System.out.println("Green Bay      8   7  1");
+		System.out.println("Chicago        8   8  0");
+		System.out.println("Detroit        7   9  0");
+		System.out.println("Minnesota      5  10  1");
+	}
+}
