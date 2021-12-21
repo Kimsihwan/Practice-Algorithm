3d6cbff5495f0c98b93f086c17c9af0a9a744818
Sihwan-Kim <namid882@gmail.com>
2021-12-21 20:26:43 +0900

    feat: [bj, 9654] 나부 함대 데이터
    https://www.acmicpc.net/problem/9654

---
+++ baekjoon/BOJ_9654.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : 나부 함대 데이터
+ * url : https://www.acmicpc.net/problem/9654
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_9654 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+		System.out.println("N2 Bomber      Heavy Fighter  Limited    21        ");
+		System.out.println("J-Type 327     Light Combat   Unlimited  1         ");
+		System.out.println("NX Cruiser     Medium Fighter Limited    18        ");
+		System.out.println("N1 Starfighter Medium Fighter Unlimited  25        ");        
+		System.out.println("Royal Cruiser  Light Combat   Limited    4         ");
+	}
+}
