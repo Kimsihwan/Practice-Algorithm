5d73147f21ffbf984b30296e8faffa5acadb840e
Sihwan-Kim <namid882@gmail.com>
2021-12-28 17:45:31 +0900

    Change code

--- baekjoon/BOJ_15727.java
+++ baekjoon/BOJ_15727.java
@@ -14,11 +14,12 @@ import java.util.Scanner;
 public class BOJ_15727 {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
-		System.out.println("N2 Bomber      Heavy Fighter  Limited    21        ");
-		System.out.println("J-Type 327     Light Combat   Unlimited  1         ");
-		System.out.println("NX Cruiser     Medium Fighter Limited    18        ");
-		System.out.println("N1 Starfighter Medium Fighter Unlimited  25        ");        
-		System.out.println("Royal Cruiser  Light Combat   Limited    4         ");
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		int rs = N / 5;
+		int remain = N % 5;
+		System.out.println(remain > 0 ? rs + 1 : rs);
+		sc.close();
 	}
 }
