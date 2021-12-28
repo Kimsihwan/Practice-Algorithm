c70db8e16fbe6b61095ab5941711f8eca0586647
Sihwan-Kim <namid882@gmail.com>
2021-12-28 17:47:28 +0900

    feat: [bj, 15733] 나는 누구인가
    https://www.acmicpc.net/problem/15733

---
+++ baekjoon/BOJ_15733.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : 나는 누구인가
+ * url : https://www.acmicpc.net/problem/15733
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_15733 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("I'm Sexy");
+	}
+}
