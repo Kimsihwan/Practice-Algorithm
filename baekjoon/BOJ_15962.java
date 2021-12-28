2159252e7704ec1a98583614024902ef19c542a4
Sihwan-Kim <namid882@gmail.com>
2021-12-28 18:07:34 +0900

    feat: [bj, 15962] 새로운 시작
    https://www.acmicpc.net/problem/15962

---
+++ baekjoon/BOJ_15962.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : 새로운 시작
+ * url : https://www.acmicpc.net/problem/15962
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_15962 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("파이팅!!");
+	}
+}
