17ac497bfaabe830b5c4d6d3d0006a209642ddb5
Sihwan-Kim <namid882@gmail.com>
2021-12-28 18:19:36 +0900

    feat: [bj, 16394] 홍익대학교
    https://www.acmicpc.net/problem/16394

---
+++ baekjoon/BOJ_16394.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : 홍익대학교
+ * url : https://www.acmicpc.net/problem/16394
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_16394 {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		System.out.println(N - 1946);
+		sc.close();
+	}
+}
