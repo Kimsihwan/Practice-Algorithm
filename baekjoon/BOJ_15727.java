c284234089d1bd899ca7f7ac228089ca0f60cfe9
Sihwan-Kim <namid882@gmail.com>
2021-12-28 17:44:36 +0900

    feat: [bj, 15727] 조별과제를 하려는데 조장이 사라졌다
    https://www.acmicpc.net/problem/15727

---
+++ baekjoon/BOJ_15727.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : 조별과제를 하려는데 조장이 사라졌다
+ * url : https://www.acmicpc.net/problem/15727
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_15727 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+		System.out.println("N2 Bomber      Heavy Fighter  Limited    21        ");
+		System.out.println("J-Type 327     Light Combat   Unlimited  1         ");
+		System.out.println("NX Cruiser     Medium Fighter Limited    18        ");
+		System.out.println("N1 Starfighter Medium Fighter Unlimited  25        ");        
+		System.out.println("Royal Cruiser  Light Combat   Limited    4         ");
+	}
+}
