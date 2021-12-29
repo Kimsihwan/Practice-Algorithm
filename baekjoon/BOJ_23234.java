f1ad8f94d7466a408de3ee8f62800fc6d1391d97
Sihwan-Kim <namid882@gmail.com>
2021-12-29 11:56:57 +0900

    feat: [bj, 23234] The World Responds
    https://www.acmicpc.net/problem/23234

---
+++ baekjoon/BOJ_23234.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : The World Responds
+ * url : https://www.acmicpc.net/problem/23234
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_23234 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("The world says hello!");
+	}
+}
