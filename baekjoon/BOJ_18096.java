d20948f826b12f03c1811e2f65d663245941ff65
Sihwan-Kim <namid882@gmail.com>
2021-12-30 10:06:54 +0900

    feat: [bj, 18301] Арифметическая магия
    https://www.acmicpc.net/problem/18096

---
+++ baekjoon/BOJ_18096.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : Арифметическая магия
+ * url : https://www.acmicpc.net/problem/18096
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_18096 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(1);
+	}
+}
