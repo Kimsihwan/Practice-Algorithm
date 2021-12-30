babd4c7c790933d62a581e69e1571d38df1309dc
Sihwan-Kim <namid882@gmail.com>
2021-12-30 10:37:25 +0900

    feat: [bj, 17295] 엔드게임 스포일러
    https://www.acmicpc.net/problem/17295

---
+++ baekjoon/BOJ_17295.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * 이름 : 엔드게임 스포일러
+ * url : https://www.acmicpc.net/problem/17295
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_17295 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Avengers: Endgame");
+	}
+}
