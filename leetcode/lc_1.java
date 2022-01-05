db9d99cd7d7b0eab6266a3368ce573fc51c0a4e4
Sihwan-Kim <namid882@gmail.com>
2022-01-05 11:04:44 +0900

    change code

--- leetcode/lc_1.java
+++ leetcode/lc_1.java
@@ -2,6 +2,7 @@ package baekjoon;
 
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
@@ -13,16 +14,33 @@ import java.util.Scanner;
  */
 public class BOJ_8393 {
 
-	public int[] twoSum(int[] nums, int target) {
-		int[] rs = null;
+	// Brute Force
+	// Time Complexity : O(n2)
+	// Space Complexity : O(1)
+	public static int[] twoSum(int[] nums, int target) {
 		for (int i = 0; i < nums.length - 1; i++) {
-			for (int j = 1; j < nums.length; j++) {
-				if (i != j && nums[i] + nums[j] == target) {
-					rs = new int[] { i, j };
-					break;
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					return new int[] { i, j };
 				}
 			}
 		}
-		return rs;
+		return null;
+	}
+	
+	// Hash Map
+	// Time Complexity : O(n)
+	// Space Complexity : O(n) => 최악의 경우 맵에 모든값을 저장해야하기 때문
+	public static int[] twoSum2(int[] nums, int target) {
+		HashMap<Integer, Integer> indexMap = new HashMap<>();
+		for(int i = 0; i < nums.length; i++) {
+			Integer requiredNum  = (Integer) (target- nums[i]);
+			if(indexMap.containsKey(requiredNum)) {
+				int toReturn[] = {indexMap.get(requiredNum), i};
+				return toReturn;
+			}
+			indexMap.put(nums[i], i);
+		}
+		return null;
 	}
 }
