e78cadcd8ccb874c1974346e643a73cb502fde51
Sihwan-Kim <namid882@gmail.com>
2022-01-09 23:56:37 +0900

    feat: [bj, 1874] 스택 수열
    https://www.acmicpc.net/problem/1874

---
+++ baekjoon/BOJ_1874.java
@@ -0,0 +1,47 @@
+
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * 이름 : 스택 수열
+ * url : https://www.acmicpc.net/problem/1874
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_1874 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		Stack<Integer> stack = new Stack<>();
+
+		int N = sc.nextInt();
+
+		int start = 0;
+
+		while (N-- > 0) {
+			int value = sc.nextInt();
+
+			if (value > start) {
+				for (int i = start + 1; i <= value; i++) {
+					stack.push(i);
+					sb.append('+').append('\n');
+				}
+				start = value;
+			}
+			//
+			else if (stack.peek() != value) {
+				System.out.println("NO");
+				return;
+			}
+
+			stack.pop();
+			sb.append('-').append('\n');
+		}
+
+		System.out.println(sb);
+		sc.close();
+	}
+}
