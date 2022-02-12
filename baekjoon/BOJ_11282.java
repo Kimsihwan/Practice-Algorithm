1ce5d191f5508667c16e78f330cb0660e5414d71
Sihwan-Kim <namid882@gmail.com>
2022-02-12 22:17:54 +0900

    feat: [bj, 11282] 한글
    https://www.acmicpc.net/problem/11282

---
+++ baekjoon/BOJ_11282.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ * 이름 : 한글
+ * url : https://www.acmicpc.net/problem/11282
+ * 
+ * @author kimsihwan
+ *
+ */
+public class BOJ_11282 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = 44031 + sc.nextInt();
+		System.out.println((char) n);
+
+		sc.close();
+
+	}
+}
