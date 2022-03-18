930c40e2d26409a3dfe00e546b7220f60e310911
Sihwan-Kim <namid882@gmail.com>
2022-03-18 21:14:13 +0900

    feat: [pm, 12925] 문자열을 정수로 바꾸기
    https://programmers.co.kr/learn/courses/30/lessons/12925

---
+++ programmers/pm_12925.java
@@ -0,0 +1,17 @@
+package programmers;
+
+/**
+ * 이름 : 문자열을 정수로 바꾸기
+ * url : https://programmers.co.kr/learn/courses/30/lessons/12925
+ * 
+ * @author kimsihwan
+ *
+ */
+public class pm_12925 {
+	static int count;
+
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+
+}
