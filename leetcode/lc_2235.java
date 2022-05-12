59404b5853076f51cc30341ba3dd401b6e08169a
Sihwan-Kim <namid882@gmail.com>
2022-05-12 15:38:12 +0900

    feat: [lc, 2235] Add Two Integers
    https://leetcode.com/problems/add-two-integers/

---
+++ leetcode/lc_2235.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 이름 : Add Two Integers
+ * url : https://leetcode.com/problems/add-two-integers/
+ * 
+ * @author kimsihwan
+ *
+ */
+public class lc_2235 {
+	public int sum(int num1, int num2) {
+		return num1+num2;
+	}
+}
