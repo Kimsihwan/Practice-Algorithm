310c94d549880876e1e5316de4050da9e16e71bb
Sihwan-Kim <namid882@gmail.com>
2023-03-19 15:32:59 +0900

    feat: [boj, 15649] N과 M (1)
    https://www.acmicpc.net/problem/15649

--- baekjoon/BOJ_15649.java
+++ baekjoon/BOJ_15649.java
@@ -1,47 +1,64 @@
-package baekjoon;
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 /**
  * 이름 : N과 M (1)
  * url : https://www.acmicpc.net/problem/15649
- * 
- * @author kimsihwan
+ * <p>
+ * 1. 아이디어
+ * - 백트래킹 재귀함수 안에서, for 돌면서 숫자 선택(이때 방문여부 확인)
+ * - 재귀함수에서 M개를 선택헸을 경우 print
+ * <p>
+ * 2. 시간복잡도
+ * - N! > 가능
+ * <p>
+ * 3. 자료구조
+ * - 결과값 저장 int[]
+ * - 방문여부 체크 bool[]
  *
+ * @author kimsihwan
  */
 public class BOJ_15649 {
 
-	input();
+    private static Stack<Integer> rs;
+    private static boolean[] chk;
+    private static Integer N;
+    private static Integer M;
+    private static String[] arr;
 
-	arr = new int[m];
-	visit = new boolean[n];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        arr = br.readLine().split(" ");
+        N = Integer.valueOf(arr[0]);
+        M = Integer.valueOf(arr[1]);
 
-	dfs(0);
-}
+        rs = new Stack<Integer>();
+        chk = new boolean[N + 1];
 
-static void dfs(int depth) {
-	// 기저 사례 : 노드의 끝에 닿았을경우
-	if (depth == m) {
-		for (int val : arr) {
-			System.out.print(val + " ");
-		}
-		System.out.println();
-		return;
-	}
+        recursive(0);
+        br.close();
+    }
 
-	for (int i = 0; i < n; i++) {
-		if(!visit[i]) {
-			visit[i] = true;
-			arr[depth] = i + 1;
-			dfs(depth+1);
-			visit[i] = false;
-		}
-	}
-}
+    public static void recursive(Integer num) {
+        if (num == M) {
+            for (int i = 0; i < rs.size(); i++) {
+                System.out.print(rs.get(i) + " ");
+            }
+			System.out.println();
+            return;
+        }
 
-static void input() {
-	Scanner sc = new Scanner(System.in);
-	n = sc.nextInt();
-	m = sc.nextInt();
-	sc.close();
+        for (int i = 1; i < N + 1; i++) {
+            if (!chk[i]) {
+                chk[i] = true;
+				rs.add(i);
+                recursive(num + 1);
+				chk[i] = false;
+				rs.pop();
+            }
+        }
+    }
+
 }
