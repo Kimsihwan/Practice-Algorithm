93eb4050cf3ac5378ba2fbe5851a590465fff4a9
Sihwan-Kim <namid882@gmail.com>
2023-05-18 18:33:56 +0900

    feat: [boj, 1436] 영화감독 숌
    https://www.acmicpc.net/problem/1436

---
+++ baekjoon/BOJ_1436.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+
+public class BOJ_1436 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int num = 666;
+        int count = 1;
+
+        while(count != N) {
+            num++;
+
+            if(String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+
+        System.out.print(num);
+    }
+}
