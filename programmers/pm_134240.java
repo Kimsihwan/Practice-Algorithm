a6a8ea5a37216031fbd81d3518b06f87f4b8c322
Sihwan-Kim <namid882@gmail.com>
2023-05-25 15:42:32 +0900

    feat: [pm, 134240] 푸드 파이트 대회
    https://school.programmers.co.kr/learn/courses/30/lessons/134240

---
+++ programmers/pm_134240.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class pm_134240 {
+    public String solution(int[] food) {
+        int[] newFood = new int[food.length-1];
+        for(int i = 0; i < newFood.length; i++) {
+            newFood[i] = food[i+1]/2;
+        }
+
+        String answer = "";
+        for(int i = 0; i < newFood.length; i++) {
+            int cnt = newFood[i];
+            while(cnt-- > 0) {
+                answer+= String.valueOf(i+1);
+            }
+        }
+
+        return answer+"0"+new StringBuilder(answer).reverse().toString();
+    }
+}
