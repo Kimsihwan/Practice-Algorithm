f8a1d80556d3f82c6f59aec67aae9ce7e9c657cf
Sihwan-Kim <namid882@gmail.com>
2023-05-30 14:22:35 +0900

    feat: [pm, 178871] 달리기 경주
    https://school.programmers.co.kr/learn/courses/30/lessons/178871

---
+++ programmers/pm_178871.java
@@ -0,0 +1,28 @@
+package programmers;
+
+public class pm_178871 {
+    public String[] solution(String[] players, String[] callings) {
+
+        int n = players.length;
+
+        HashMap<String, Integer> indexMap = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+            indexMap.put(players[i], i);
+        }
+
+        for(String call : callings) {
+            int idx = indexMap.get(call);
+            if(idx > 0) {
+                String tmp = players[idx - 1];
+                players[idx - 1] = players[idx];
+                players[idx] = tmp;
+
+                indexMap.put(players[idx-1], idx-1);
+                indexMap.put(players[idx], idx);
+            }
+        }
+
+        return players;
+    }
+}
