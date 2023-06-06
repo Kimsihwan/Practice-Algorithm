424282d432143058f458aec0a4c8578f3c6eb401
Sihwan-Kim <namid882@gmail.com>
2023-06-06 17:31:31 +0900

    feat: [pm, 12953] N개의 최소공배수
    https://school.programmers.co.kr/learn/courses/30/lessons/12953

---
+++ programmers/pm_12953.java
@@ -0,0 +1,31 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class pm_12953 {
+	public int solution(int[] arr) {
+		int answer = 0;
+
+		if(arr.length == 1) return arr[0];
+
+		int gcd = gcd(arr[0], arr[1]);
+		answer = (arr[0] * arr[1]) / gcd;
+
+		if(arr.length > 2) {
+			for(int i = 2; i < arr.length; i++){
+				gcd = gcd(answer, arr[i]);
+				answer = (answer * arr[i]) / gcd;
+			}
+		}
+
+
+		return answer;
+	}
+
+	public int gcd(int a, int b) {
+		if(b == 0) return a;
+		else return gcd(b, a%b);
+	}
+
+}
