a941c23b2af56fa7b9cb349e0a8f1ab819d6d9bf
Sihwan-Kim <namid882@gmail.com>
2023-06-07 14:04:04 +0900

    feat: [boj, 131701] 연속 부분 수열 합의 개수
    https://school.programmers.co.kr/learn/courses/30/lessons/131701

---
+++ programmers/pm_131701.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.*;
+
+public class pm_131701 {
+    public int solution(int[] elements) {
+        int[] newElements = new int[elements.length*2];
+
+        for(int i = 0; i < elements.length; i++) {
+            newElements[i] = newElements[i+elements.length] = elements[i];
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < elements.length; i++) {
+            int sum = 0;
+            for(int j = i; j < i+elements.length; j++) {
+                sum += newElements[j];
+                set.add(sum);
+            }
+        }
+
+        return set.size();
+    }
+}
