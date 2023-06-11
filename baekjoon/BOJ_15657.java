10de620cf885ab825e3764e65053bfc688e523e9
Sihwan-Kim <namid882@gmail.com>
2023-06-11 15:46:33 +0900

    feat: [boj, 15657] N과 M (8) https://www.acmicpc.net/problem/15657

---
+++ baekjoon/BOJ_15657.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_15657 {
+    static int N;
+    static int M;
+    static int[] ans;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+        ans = new int[M];
+        arr = new int[N];
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0);
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int k) {
+        if(k == M) {
+            for(int v : ans) {
+                sb.append(v).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < N; i++) {
+            ans[k] = arr[i];
+            dfs(k+1);
+        }
+    }
+}
